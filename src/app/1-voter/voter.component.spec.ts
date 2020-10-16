d8142c934a6308c177100a5fd8743d04cb803e31
Wolodja <wolodjawaskovych@i.ua>
2020-10-16 14:04:01 +0200

    Testing porperty bindings

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,3 +1,4 @@
+import { By } from '@angular/platform-browser';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { VoterComponent } from './voter.component';
 
@@ -15,6 +16,27 @@ describe('VoterComponent', () => {
     component = fixture.componentInstance;
   });
 
-  it('', () => {
+  it('should render total votes', () => {
+
+    component.othersVote = 20;
+    component.myVote = 1;
+
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.vote-count'));
+    let el : HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain('21');
+  });
+
+
+  it('should highlight the upvote button if upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+    let el : HTMLElement = de.nativeElement;
+
+    expect(el.classList).toContain('highlighted');
   });
 });
